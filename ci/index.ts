53eaebfbe877869dfeded34fff9b6ca43299e8d5
Matheus Augusto <auguzsto@gmail.com>
2026-01-30 14:32:15 -0300

    feat: Listando apenas arquivos permitidos para compilação

--- ci/index.ts
+++ ci/index.ts
@@ -1,4 +1,16 @@
-const CHANGED_FILES = process.env.CHANGED_FILES!
-const ARRAY_CHANGED_FILES = CHANGED_FILES.split("\n")
+import path from "node:path"
 
-console.log(ARRAY_CHANGED_FILES)
+const CHANGED_FILES = process.env.CHANGED_FILES!
+let ARRAY_CHANGED_FILES = CHANGED_FILES.split("\n")
+
+for (const filename of ARRAY_CHANGED_FILES) {
+    let extension = path.extname(filename)
+    let allow = [".cls", ".int", ".csp", ".mac"]
+    let isNotExtensionAllow = (!allow.includes(extension))
+
+    if (isNotExtensionAllow) {
+        continue
+    }
+
+    console.log(filename)
+}
