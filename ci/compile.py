f5f0fd9ac74170e885214a75dd8a3ec4df0f671c
Matheus Augusto <auguzsto@gmail.com>
2026-01-30 15:55:57 -0300

    refactor: ci/compile.py

--- ci/compile.py
+++ ci/compile.py
@@ -4,24 +4,33 @@ import json
 ARQUIVOS_ALTERADOS = os.environ["CHANGED_FILES"]
 ARRAY_ARQUIVOS_ALTERADOS = ARQUIVOS_ALTERADOS.split("\n")
 
+def main():
+    extrairConteudoArquivoAlterado()
+
+def extrairConteudoArquivoAlterado():
+    for filename in ARRAY_ARQUIVOS_ALTERADOS:
+        conteudo = []
+        
+        with open(filename, "r", encoding="utf-8") as file:
+            if not isExtencaoPermitida(filename):
+                continue
+            while linha := file.readline():
+                conteudo.append(linha.rstrip("\n"))
+
+    conteudoSerializado = json.dumps(conteudo, ensure_ascii=False, indent=2)
+
+    # Futuramento esse conteúdo será enviado para um API para realizar o deploy
+    # no servidor.
+    print(conteudoSerializado)
+
 def isExtencaoPermitida(filename):
-    permitidos = [".cls", ".mac", ".csp", ".int"]
+    permitidas = [".cls", ".mac", ".csp", ".int"]
     _, extencao = os.path.splitext(filename)
 
-    for permitido in permitidos:
-        if extencao == permitido:
+    for permitida in permitidas:
+        if extencao == permitida:
             return True
         
     return False
 
-for filename in ARRAY_ARQUIVOS_ALTERADOS:
-    conteudo = []
-    
-    with open(filename, "r", encoding="utf-8") as file:
-        if not isExtencaoPermitida(filename):
-            continue
-        while linha := file.readline():
-            conteudo.append(linha.rstrip("\n"))
-
-    conteudoSerializado = json.dumps(conteudo, ensure_ascii=False, indent=2)
-    print(conteudoSerializado)
+main()
